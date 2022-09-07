38d20349e38a75b00e529bb54d3cb60a6bc87e4d
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-09-07 21:45:13 +0100

    setting up local stotrage for saving popular recipes, debugging api request error

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -11,34 +11,49 @@ function Popular() {
   }, []);
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
-    const data = await api.json();
-    console.log(data);
-    setPopular(data.recipes);
-    console.log(data.recipes);
+    const check = localStorage.getItem("popular");
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      console.log(data);
+      setPopular(data.recipes);
+      console.log(data.recipes);
+    }
   };
 
   return (
     <div>
-      return (
       <Wrapper>
         <h3>Trending</h3>
-        <Splide>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
           {popular.map((recipe) => {
             return (
-              <SplideSlide>
+              <SplideSlide key={recipe.id}>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
                 </Card>
               </SplideSlide>
             );
           })}
         </Splide>
       </Wrapper>
-      );
     </div>
   );
 }
@@ -50,10 +65,40 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: "absolute";
+  width: 100%;
+  height: 100%;
+  bacground: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
 `;
 
 export default Popular;
