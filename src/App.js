f1bdddd70b3dfab2a68c09065af6f6c14af21a62
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-09-20 19:21:57 +0100

    added links with different cuisines to Category.jsx and set up routing between the links with React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Pages from './pages/Pages.jsx';
+import Pages from "./pages/Pages.jsx";
+import Category from "./components/Category.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <Pages/>
+      <BrowserRouter>
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import Home from './Home'
+import React from "react";
+import Home from "./Home";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Cuisine from "./Cuisine";
 
 function Pages() {
   return (
-    <Home/>
-  )
+  
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cusine/:type" element={<Cuisine />} />
+      </Routes>
+
+  );
 }
 
-export default Pages
+export default Pages;
