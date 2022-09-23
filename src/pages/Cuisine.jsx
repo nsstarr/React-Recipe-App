e2f5fcd444c7ab8ffd9df522c34dcac2c59b5ab0
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-09-23 16:08:50 +0100

    Working on the Cuisine component's routing. Debugging - no routes matched location

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useState} from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 function Cuisine() {
-  return (
-    <div>Cuisine</div>
-  )
+  const [cuisine, setCuisine] = useState([])
+  let params = useParams()
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results)
+    console.log(recipes.results)
+  };
+
+  useEffect(() => {
+    getCuisine(params.type)
+    console.log(params.type)
+  }, [params.type])
+
+  return <div></div>;
 }
 
-export default Cuisine
+export default Cuisine;
